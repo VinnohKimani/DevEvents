e4392fed4f65d90e0e4730cd02f1122ee066821d
Vincent Macharia Kimani <vincentmachariakimani@gmail.com>
2026-08-11 19:26:57 +0300

    updated and cleaned respective layouts

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,66 +1,15 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "@mantine/core/styles.css";
-import { ColorSchemeScript, createTheme, MantineProvider } from "@mantine/core";
-import LightRays from "@/components/LightRays";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Dev Events",
-  description: "The Hub for Every Dev Event You Mustn't Miss!!",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const theme = createTheme({
-    fontFamily: "Open Sans, sans-serif",
-    primaryColor: "cyan",
-    autoContrast: true,
-  });
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-      suppressHydrationWarning
-    >
-      <head>
-        {/* Reads the user's local system preference and understands whether it is dark or light theme  */}
-        <ColorSchemeScript />
-      </head>
+    <html lang="en" suppressHydrationWarning>
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
-        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
-          <LightRays
-            raysOrigin="top-center-offset"
-            raysColor="#5dfeca"
-            raysSpeed={0.5}
-            lightSpread={0.9}
-            rayLength={1.4}
-            followMouse={true}
-            mouseInfluence={0.02}
-            noiseAmount={0.0}
-            distortion={0.01}
-            className="custom-rays"
-            pulsating={false}
-            fadeDistance={1}
-            saturation={1}
-          />
-        </div>
-        <MantineProvider theme={theme} defaultColorScheme="dark">
-          {children}
-        </MantineProvider>
+        <main>{children}</main>
       </body>
     </html>
   );
