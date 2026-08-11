7a9adbc1d6b366d5d2b91a41e32427a7ca747ac5
Vincent Macharia Kimani <vincentmachariakimani@gmail.com>
2026-08-11 18:38:19 +0300

    implemented rolebased authentication proxy  and update route matcher configuration

--- proxy.ts
+++ proxy.ts
@@ -3,12 +3,34 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function proxy(request: NextRequest) {
-  return NextResponse.redirect(new URL("/home", request.url));
+    const {pathname} = request.nextUrl;
+
+     const isAuthenticated  = request.cookies.get("auth_session")?.value == "true";
+     const userRole = request.cookies.get("user_role")?.value;
+
+     if(pathname.startsWith("/admin")){
+        if(!isAuthenticated){
+            return NextResponse.redirect(new URL("/login", request.url))
+        }
+        if(userRole !== "admin"){
+             return NextResponse.redirect(new URL("/dashboard", request.url));
+        }
+     }
+     if(pathname.startsWith("/dashboard")){
+        if(!isAuthenticated){
+            return NextResponse.redirect(new URL("/login", request.url));
+        }
+     }
+     if((pathname == "/login" || pathname === "/signup") && isAuthenticated){
+        return NextResponse.redirect(new URL("/dashboard", request.url))
+     }
+
+  return NextResponse.next()
 }
 
 // Alternatively, you can use a default export:
 // export default function proxy(request: NextRequest) { ... }
 
 export const config = {
-  matcher: "/about/:path*",
+  matcher: ["/dashboard/:path", "/admin/:path", "/login", "/signup"],
 };
